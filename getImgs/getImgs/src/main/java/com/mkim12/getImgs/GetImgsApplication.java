8a538595b249597803e2f4220783487ffc98d163
김민호 <mkim12@ebay.com>
2018-09-03 10:48:31 +0900

    commit

---
+++ getImgs/getImgs/src/main/java/com/mkim12/getImgs/GetImgsApplication.java
@@ -0,0 +1,12 @@
+package com.mkim12.getImgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetImgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetImgsApplication.class, args);
+	}
+}
---
+++ getImgs/getImgs/src/main/java/com/mkim12/getImgs/Load/LoadController.java
@@ -0,0 +1,9 @@
+package com.mkim12.getImgs.Load;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoadController {
+
+}
