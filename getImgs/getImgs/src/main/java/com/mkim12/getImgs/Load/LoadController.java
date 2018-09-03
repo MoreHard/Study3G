aff7b9bef118fa3512cb2b7ec9d17272820425b4
김민호 <mkim12@ebay.com>
2018-09-03 13:18:59 +0900

    HtmlUnit 테스트

--- getImgs/getImgs/src/main/java/com/mkim12/getImgs/Load/LoadController.java
+++ getImgs/getImgs/src/main/java/com/mkim12/getImgs/Load/LoadController.java
@@ -1,9 +1,37 @@
 package com.mkim12.getImgs.Load;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/load")
 public class LoadController {
+    WebClient webClient;
+
+    LoadController() {
+        webClient = new WebClient(BrowserVersion.CHROME);
+
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+    }
+
+    @GetMapping("/test")
+    public void Load() {
+        String url = "http://www.auction.co.kr";
+        try {
+            HtmlPage page = (HtmlPage)webClient.getPage(url);
+
+            System.out.println(page);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
 
 }
